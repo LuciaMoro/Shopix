0087728744fe3ea9dc20c5fb56420402ee8e04c4
Lucia Enache <luciaenache@Lucias-MacBook-Air.local>
2025-05-08 18:55:57 +0300

    add product component

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,11 +1,28 @@
+import React, { useEffect, useState } from "react";
+import Product from "./components/Product";
+
 function Products() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await fetch("https://fakestoreapi.in/api/products");
+    const data = await response.json();
+    setProducts(data.products);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <>
-      <h1>Product List</h1>
-      <div className="card">
-        <p></p>
-      </div>
-    </>
+    <div>
+      <h2>Products</h2>
+      <ul>
+        {products.map(({ id, title, price }) => (
+          <Product key={id} title={title} price={price} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
